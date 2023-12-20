cf15d8543297e3f0987818cd0bd6d9fe1d3ed70a
Nguyên Trần Văn <102210175@sv1.dut.udn.vn>
2023-12-20 14:38:38 +0700

    sdf

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+
+app.get('/api', (req, res) => {
+    res.send({
+        people:" asdasd"
+    })
+})
+
+app.get('/.well-known/pki-validation/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'BA5506CE7CAA3179262CEDC197D50FA8.txt'))
+})
+
+app.listen(port, () => console.log("helo kitty"))
